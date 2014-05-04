df23c86c0d3b2b06583d325fd6b04009c31543be
John Cipriano <johnmikecip@gmail.com>
2014-05-03 23:19:52 -0700

    primestest

--- Util/PrimesTest.java
+++ Util/PrimesTest.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
