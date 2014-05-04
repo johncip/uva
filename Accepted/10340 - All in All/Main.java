fd05335d05e6c083483cf6834aa44892676730df
John Cipriano <johnmikecip@gmail.com>
2014-05-03 20:07:47 -0700

    solved all in all

---
+++ Accepted/10340 - All in All/Main.java
@@ -0,0 +1,54 @@
+/*
+ * 10340 - All in All
+ * 
+ * Keep an index on the plaintext and look at each character in the
+ * ciphertext, incrementing the index when the current character matches the
+ * indexed one.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Entry point. For each test case, prints out whether the plaintext appears
+     * in the ciphertext.
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        while (in.hasNext()) {
+            String ptext = in.next();
+            String ctext = in.next();
+
+            if (check(ptext, ctext))
+                sb.append("Yes\n");
+            else
+                sb.append("No\n");
+
+        }
+
+        System.out.print(sb);
+        in.close();
+    }
+
+    /**
+     * Returns true if each character of the plaintext is found in the same
+     * relative order in the ciphertext.
+     */
+    private static boolean check(String ptext, String ctext) {
+        int i = 0;
+
+        for (int j = 0; j < ctext.length(); j++) {
+            if (ctext.charAt(j) == ptext.charAt(i))
+                i++;
+
+            if (i == ptext.length())
+                return true;
+        }
+
+        return false;
+    }
+
+}
